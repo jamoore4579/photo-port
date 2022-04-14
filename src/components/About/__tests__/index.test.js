816ed389f1f672e1dc8a9cd50aa28bfae1d6d0ae
Irish Moore <jasonamoore@frontier.com>
2022-04-13 20:23:31 -0400

    update files

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,28 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
-
 import { render, cleanup } from '@testing-library/react';
-
 import '@testing-library/jest-dom/extend-expect';
-
 import About from '..';
 
 afterEach(cleanup);
 
 describe('About component', () => {
-    //renders About test
+  it('renders', () => {
+    render(<About />);
+  });
 
-    // First Test
-    it('renders', () => {
-        render(<About />);
-    });
+  it('matches snapshot', () => {
+    const { asFragment } = render(<About />);
 
-    // Second Test
-    it('matches snapshot DOM node structure', () => {
-        // render About
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    })
-
-  })
-
-  
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
